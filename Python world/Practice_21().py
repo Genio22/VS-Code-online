22d17f9551e7c318fe5d20020a5664fbaec6fabf
Genio22 <relaquantisten@gmail.com>
2024-02-06 02:53:49 +0600

    first commit

---
+++ Python world/Practice_21().py
@@ -0,0 +1,11 @@
+list2 = []
+num = []
+for _ in range(int(input())):
+    try:
+        name = input()
+        number = float(input())
+        lists = [[name, number]]
+        list2 +=lists
+        print(list2)
+    except Exception as e:
+        print ('correct it: ' ,e)
